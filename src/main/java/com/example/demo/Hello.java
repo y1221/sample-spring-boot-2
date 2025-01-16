894ea375971cadb9ef55a39f0929c5aef38b83c1
okayu <yuto.gio.0515@gmail.com>
2025-01-16 20:44:01 +0900

    テンプレート、Modelを使用してビューを表示

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -1,14 +1,17 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class Hello {
 
     @GetMapping("/")
-    public String hello() {
-        return "Hello Sample Spring Boot 2！";
+    public String hello(Model model) {
+        String message = "Hello Sample Spring Boot 2！";
+        model.addAttribute("message", message);
+        return "hello";
     }
 
 }
