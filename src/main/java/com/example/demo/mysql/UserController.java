d3f595813c6f29150847994668a74f861a68b0ae
okayu <yuto.gio.0515@gmail.com>
2025-01-20 22:02:26 +0900

    追加機能実装

--- src/main/java/com/example/demo/mysql/UserController.java
+++ src/main/java/com/example/demo/mysql/UserController.java
@@ -1,9 +1,12 @@
 package com.example.demo.mysql;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping(path = "/user")
@@ -14,11 +17,28 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
+    // 一覧表示
     @GetMapping({ "", "/" })
     public ModelAndView index(ModelAndView mav) {
         Iterable<User> users = userRepository.findAll();
         mav.addObject("users", users);
         mav.setViewName("user");
         return mav;
+    }
+
+    // ユーザー登録
+    @GetMapping("/add")
+    public ModelAndView add(ModelAndView mav) {
+        User user = new User();
+        mav.addObject("newUser", user);
+        mav.setViewName("add");
+        return mav;
+    }
+
+    @PostMapping("/add")
+    @Transactional(readOnly = false)
+    public ModelAndView add(@ModelAttribute("newUser") User user) {
+        userRepository.saveAndFlush(user);
+        return new ModelAndView("redirect:/user");
     }
 }
